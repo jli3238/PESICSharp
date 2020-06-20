9e3cead81381619fa3dd8039072f267eaa97816c
jli3238 <jli3238@gmail.com>
2020-06-20 11:17:43 -0500

    Rename

--- PESICSharp/Program.cs
+++ PESICSharp/Program.cs
@@ -12,17 +12,17 @@ namespace PESICSharp
             {
                 Console.WriteLine("Please enter a string: ");
                 string inputString = Console.ReadLine();
-                string outputStringBySystemRandomCall = rAS.RandomizeBySystemRandomCall(inputString);
-                Console.WriteLine("Output randomized string by System.Random call is: " + outputStringBySystemRandomCall);
-                string outputStringByCustomMethodCall = rAS.RandomizeByCustomMethodCall(inputString);
-                Console.WriteLine("Output randomized string by custom random call is: " + outputStringByCustomMethodCall);
+                string outputStringBySystemRandomFYShuffleCall = rAS.RandomizeBySystemRandomFYShuffleCall(inputString);
+                Console.WriteLine("Output randomized string by SystemRandomFYShuffle call is: " + outputStringBySystemRandomFYShuffleCall);
+                string outputStringBySystemRandomSortCall = rAS.RandomizeBySystemRandomSortCall(inputString);
+                Console.WriteLine("Output randomized string by SystemRandomSort call is: " + outputStringBySystemRandomSortCall);
             }
         }
     }
 
     public class RandomizeAString
     {
-        public string RandomizeBySystemRandomCall(string inputString)
+        public string RandomizeBySystemRandomFYShuffleCall(string inputString)
         {
             Random RNG = new Random();
             Char[] chars = inputString.ToCharArray();
@@ -36,7 +36,7 @@ namespace PESICSharp
             return new string(chars);
         }
 
-        public string RandomizeByCustomMethodCall(string inputString)
+        public string RandomizeBySystemRandomSortCall(string inputString)
         {
             Random RNG = new Random();
             char[] charList = inputString.ToCharArray();
@@ -44,26 +44,15 @@ namespace PESICSharp
             return new string(charList.ToArray());
         }
 
-        public int btoi(byte[] bytes)
-        {
-            int shift = 0;
-            int res = 0;
-            foreach (byte b in bytes)
-            {
-                res += ((int)b) << 8 * (shift++);
-            }
-            return res;
-        }
-
         // Comparison of the two implementations:
-        // RandomizeBySystemRandomCall: It uses the standard Random provided in c# along with the Fisher-Yates Shuffle. 
-        //                              The standard RNG is not quite a perfect random and there is evidence that it could be better. 
-        //                              The Fisher-Yates Shuffle, however, is optimal in being an efficient uniform shuffle (i.e. all 
-        //                              permutations have an equal probability). The time complexity is O(n), assuming random number 
-        //                              generating is of O(1).
-        // RandomizeByCustomMethodCall: This one also uses Random. It assigns each element in the array with a random element in a toset 
-        //                              as a "key". As the key is part of a toset, we sort it to create an alternative order. 
-        //                              This is also uniformly random, but the time complexity is O(n log(n)), assuming random number 
-        //                              generating is of less than O(log(n)).
+        // RandomizeBySystemRandomFYShuffleCall: It uses the standard Random provided in c# along with the Fisher-Yates Shuffle. 
+        //                                       The standard RNG is not quite a perfect random and there is evidence that it could be better. 
+        //                                       The Fisher-Yates Shuffle, however, is optimal in being an efficient uniform shuffle (i.e. all 
+        //                                       permutations have an equal probability). The time complexity is O(n), assuming random number 
+        //                                       generating is of O(1).
+        // RandomizeBySystemRandomSortCall: This one also uses Random. It assigns each element in the array with a random element in a toset 
+        //                                  as a "key". As the key is part of a toset, we sort it to create an alternative order. 
+        //                                  This is also uniformly random, but the time complexity is O(n log(n)), assuming random number 
+        //                                  generating is of less than O(log(n)).
     }
 }
